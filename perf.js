1e39ffbf4138e94130ff3f412558b9992d2de379
Jason Klotzer <jklotzer@google.com>
2025-01-21 20:27:30 -0600

    add Copyright to each source file

--- perf.js
+++ perf.js
@@ -1,4 +1,19 @@
-const util = require("util");
+/*
+ Copyright 2025 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 const { DEBUG_MODE } = require("./utils");
 
 const HOT_SPOT_MIN = 100; // amount in ms to determine if there's a hot spot
@@ -36,7 +51,6 @@ class PerfCtx {
 
   print() {
     if (DEBUG_MODE) {
-      // const output = util.inspect(this.get(), { showHidden: false, depth: null, colors: true });
       const output = JSON.stringify(this.get());
       console.log(output);
     }
