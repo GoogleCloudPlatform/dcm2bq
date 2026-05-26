bb48e1d9e7cfa445a60981aeaccf5e3b721038ad
Jason Klotzer <jklotzer@google.com>
2026-05-26 22:41:29 +0000

    feat: Add license headers to various JS files

--- admin-console/backend/src/index.js
+++ admin-console/backend/src/index.js
@@ -1,3 +1,19 @@
+/*
+ Copyright 2025 Google LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      https://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
@@ -1381,13 +1397,13 @@ function normalizeStudyModalities(value) {
 
 // Search filter builder for studies/instances
 function buildSearchFilter(key, value) {
-  const whitelistedKeys = new Set([
+  const allowedKeys = new Set([
     "PatientID", "PatientName", "StudyInstanceUID", "AccessionNumber",
     "ReferringPhysicianName", "InstitutionName", "Modality", "StudyDate",
     "StudyDescription", "SeriesDescription",
   ]);
 
-  if (!whitelistedKeys.has(key)) {
+  if (!allowedKeys.has(key)) {
     const err = new Error(`Unsupported search key: ${key}`);
     err.code = 400;
     throw err;
